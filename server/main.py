998b6bd37d3b16cec51f161435d5034407028d16
kenny tan <kjtan3@dons.usfca.edu>
2023-10-29 23:57:11 -0700

    post and get review with genre, delete review

--- server/main.py
+++ server/main.py
@@ -17,7 +17,7 @@ def db_conn():
     return conn
 
 # Post a review
-@app.route("/reviews", methods=['POST'])
+@app.route("/reviews", methods=['POST']) # default method GET
 def post_review():
     if request.method == "POST":
         try:
@@ -26,6 +26,7 @@ def post_review():
             book_name = data.get("book_name")
             book_author = data.get("book_author")
             book_review = data.get("book_review")
+            book_genres = data.get("book_genres")
 
             # Establish a database connection
             conn = db_conn()
@@ -33,10 +34,20 @@ def post_review():
 
             # Insert the review data into reviews table
             cur.execute(
-                "INSERT INTO reviews (book_name, book_author, book_review) VALUES (%s, %s, %s)",
+                "INSERT INTO reviews (book_name, book_author, book_review) VALUES (%s, %s, %s) RETURNING book_id",
                 (book_name, book_author, book_review)
             )
 
+            # Retrieve the book_id
+            book_id = cur.fetchone()[0]
+            print(book_id)
+
+            for genre_name in book_genres:
+                cur.execute(
+                    "INSERT INTO bookgenres (book_id, genre_name) VALUES (%s, %s)",
+                    (book_id, genre_name)
+                )
+            
             # Commit the sql query to the database
             conn.commit()
             
@@ -47,10 +58,10 @@ def post_review():
             # Return json object saying success
             return jsonify({"message": f"Review for {book_name} created successfully"}), 201
         except Exception as e:
-            return jsonify({'error': str(e)}), 500
+            return jsonify({"error": str(e)}), 500
         
 # Get all reviews
-@app.route("/reviews") # default method GET
+@app.route("/reviews")
 def get_reviews():
     if request.method ==  "GET":
         try:
@@ -68,10 +79,22 @@ def get_reviews():
             reviews = []
 
             for record in review_records:
+                # Retrieve genres
+                book_id = record[0]
+                genre_names = []
+                cur.execute(
+                    f"SELECT * FROM bookgenres WHERE book_id = {book_id}"
+                )
+                book_genres = cur.fetchall()
+                if book_genres:
+                    genre_names = [item[2] for item in book_genres]
+
+                # Create a review entry
                 review = {
                     "book_name": record[1], # get all data from each tuple
                     "book_author": record[2],
-                    "book_review": record[3]
+                    "book_review": record[3],
+                    "book_genres": genre_names
                 }
                 reviews.append(review)
             
@@ -80,8 +103,35 @@ def get_reviews():
 
             return jsonify(reviews)
         except Exception as e:
-            return jsonify({'error': str(e)}), 500
+            return jsonify({"error": str(e)}), 500
 
+# Delete a review using book_id
+@app.route("/reviews/<int:book_id>", methods=["DELETE"])
+def delete_review(book_id):
+    if request.method == "DELETE":
+        try:
+            conn = db_conn()
+            cur = conn.cursor()
+
+            cur.execute(
+                "DELETE FROM reviews WHERE book_id = %s RETURNING book_name",
+                (book_id,)
+            )
+            book_name = cur.fetchone()[0]
+
+            cur.execute(
+                "DELETE FROM bookgenres WHERE book_id = %s",
+                (book_id,)
+            )
+
+            conn.commit()
+
+            cur.close()
+            conn.close()
+
+            return jsonify({"message": f"Review for '{book_name}' deleted."}), 201
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
