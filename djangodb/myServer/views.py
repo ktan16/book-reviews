e5d0716df58bf194297977999651df7337a686a3
Kenny Tan <kjtan3@dons.usfca.edu>
2023-10-30 23:50:10 -0700

    django db post review functionality

---
+++ djangodb/myServer/views.py
@@ -0,0 +1,42 @@
+import json
+from django.http import JsonResponse
+from .models import Reviews, Bookgenres
+
+# Create your views here.
+
+# Post a review
+def post_review(request):
+    if request.method == "POST":
+        try:
+            # Exctract data from request
+            data = json.loads(request.body)
+            book_name = data.get("book_name")
+            book_author = data.get("book_author")
+            book_review = data.get("book_review")
+            book_genres = data.get("book_genres")
+
+            # Create a new review
+            Reviews.objects.create(
+                book_name=book_name,
+                book_author=book_author,
+                book_review=book_review
+            )
+
+            # Need to get the book_id
+            new_review = Reviews.objects.get(
+                book_name=book_name,
+                book_author=book_author,
+                book_review=book_review
+            )
+            book_id = new_review.pk
+
+            # Add genres
+            for genre_name in book_genres:
+                Bookgenres.objects.create(
+                    book_id=book_id,
+                    genre_name=genre_name
+                )
+
+            return JsonResponse({"message": f"Review for {book_name} created successfully"}, status=201) 
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
